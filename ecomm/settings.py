1df589739789c231dea1dfd3b6585e926dc08e7a
Mihir-solanki-13 <76053066+Mihir-solanki-13@users.noreply.github.com>
2023-06-25 12:22:14 +0530

    Update settings.py

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -149,14 +149,14 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# # sending email through smtp 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# sending email through smtp 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 # # here enter your email id and password generated for your email
 # EMAIL_HOST_USER = EMAIL_HOST_USER
 # EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
@@ -167,10 +167,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 # set up your user and password
-# EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
-# EMAIL_HOST_USER = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = 'twqvhfpkecnaajks'
-EMAIL_HOST_USER = '13mihir2001@gmail.com'
+
 EMAIL_PORT = 587
 ############################
 
