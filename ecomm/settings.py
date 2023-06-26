e2022fce86df8625e3723a2d650c009876db2acf
Mihir-solanki-13 <76053066+Mihir-solanki-13@users.noreply.github.com>
2023-06-26 17:24:49 +0530

    Update settings.py

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -161,8 +161,8 @@ EMAIL_PORT = 587
 # EMAIL_HOST_USER = EMAIL_HOST_USER
 # EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
